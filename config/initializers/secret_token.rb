f92e59a1faecffd74ed6d5090862b50cb230e296
Ana <a.ciocarlan.12@aberdeen.ac.uk>
2014-05-02 14:12:32 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamenetwork::Application.config.secret_token = 'fd89c1f60401f9470fadcd9013562aa09f582ebb26f2f31c408eef07d96c702d9d47aad517fe47dbfeabbaa54b53e53ed34e2343b8f850cfed8908b9ef54fc5a'
